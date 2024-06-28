0a65baceb5c1cd49adc2cd52be1956c6dac9465d
Youlixx <combey.theo@hotmail.com>
2024-06-28 23:29:38 +0200

    Added cmake build command to the setup

--- setup.py
+++ setup.py
@@ -1,19 +1,106 @@
-import numpy
-from Cython.Build import cythonize
-from setuptools import setup
-from setuptools.extension import Extension
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
 
 with open("README.md", "r") as file:
     long_description = file.read()
 
 
-ext_module_wrapper = Extension(
-    "cynes.emulator",
-    ["cynes/emulator.pyx"],
-    language="c++",
-    extra_compile_args=["-std=c++11"],
-    extra_link_args=["-std=c++11"]
-)
+class CMakeExtension(Extension):
+    """CMake extension."""
+
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        """Initialize the extension.
+
+        Args:
+            name (str): Name of the extension.
+            sourcedir (str, optional): C++ source directory.
+        """
+        super().__init__(name, sources=[])
+
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
+
+
+class CMakeBuild(build_ext):
+    """CMake build extension."""
+
+    PLAT_TO_CMAKE = {
+        "win32": "Win32",
+        "win-amd64": "x64",
+        "win-arm32": "ARM",
+        "win-arm64": "ARM64",
+    }
+
+    def build_extension(self, ext: CMakeExtension) -> None:
+        """Build the shared python binaries.
+
+        Args:
+            ext (CMakeExtension): Builder extension.
+        """
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
+        extdir = ext_fullpath.parent.resolve()
+
+        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
+        cfg = "Debug" if debug else "Release"
+
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",
+        ]
+
+        build_args = []
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
+        cmake_args += [f"-DPYTHON_MODULE_VERSION={self.distribution.get_version()}"]
+
+        if self.compiler.compiler_type == "msvc":
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't contain
+            # a backward-compatibility arch spec already in the generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", CMakeBuild.PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                build_args += ["--config", cfg]
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+                ]
+
+        if sys.platform.startswith("darwin"):
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+
+            if archs:
+                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            if hasattr(self, "parallel") and self.parallel:
+                build_args += [f"-j{self.parallel}"]
+
+        build_temp = Path(self.build_temp) / ext.name
+
+        if not build_temp.exists():
+            build_temp.mkdir(parents=True)
+
+        subprocess.run(
+            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True
+        )
+
+        subprocess.run(
+            ["cmake", "--build", ".", *build_args], cwd=build_temp, check=True
+        )
 
 setup (
     name = "cynes",
@@ -26,22 +113,11 @@ setup (
     license_files =("LICENSE"),
     license="GPL-3.0",
     url="https://github.com/Youlixx/cynes",
-    setup_requires=[
-        "cython",
-        "numpy"
-    ],
-    install_requires=[
-        "numpy",
-        "pysdl2-dll",
-        "pysdl2"
-    ],
-    ext_modules = cythonize(ext_module_wrapper, language_level=3),
-    packages=[
-        "cynes"
-    ],
-    include_dirs=[
-        numpy.get_include()
-    ],
+    setup_requires=["numpy"],
+    install_requires=["numpy"],
+    ext_modules=[CMakeExtension("cynes.emulator")],
+    cmdclass={"build_ext": CMakeBuild},
+    packages=["cynes"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",

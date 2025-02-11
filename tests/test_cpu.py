45f07e7363b98373f39e447fa8e857b27c82a7ce
Youlixx <combey.theo@hotmail.com>
2025-02-11 22:26:06 +0000

    Added test_cpu_dummy_reads tests

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -32,7 +32,6 @@ def test_blargg_nes_cpu_test5(path_rom: str, frame_count: int) -> None:
         character_map=CHARACTER_MAP_DEFAULT
     )
 
-
 @pytest.mark.parametrize(
     "path_rom,frame_count", [
         ("branch_timing_tests/1.Branch_Basics.nes", 13),
@@ -54,4 +53,23 @@ def test_branch_timing_tests(path_rom: str, frame_count: int) -> None:
         ),
         expected_frame_count=frame_count,
         character_map=CHARACTER_MAP_RESTRAINED
+    )
+
+@pytest.mark.parametrize(
+    "path_rom,frame_count", [("cpu_dummy_reads/cpu_dummy_reads.nes", 43)]
+)
+def test_cpu_dummy_reads(path_rom: str, frame_count: int) -> None:
+    """Run the cpu_dummy_reads test suite."""
+    run_test_rom_ppu(
+        path_rom=path_rom,
+        success_matcher=Matcher(
+            string="Passed",
+            condition=MatchCondition.LAST_LINE_STRICT
+        ),
+        failure_matcher=Matcher(
+            string="Failed",
+            condition=MatchCondition.CONTAINS
+        ),
+        expected_frame_count=frame_count,
+        character_map=CHARACTER_MAP_DEFAULT
     )

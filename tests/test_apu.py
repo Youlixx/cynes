56c4e4e1e9fbb8e7e5effb3314ce7f08df91346c
Youlixx <combey.theo@hotmail.com>
2025-09-19 22:11:48 +0000

    Added blargg_apu_2005.07.30 tests

--- tests/test_apu.py
+++ tests/test_apu.py
@@ -2,7 +2,13 @@
 
 import pytest
 
-from tests.utils.test_rom import run_test_rom_ram
+from tests.utils.test_rom import (
+    MatchCondition,
+    Matcher,
+    run_test_rom_ppu,
+    run_test_rom_ram,
+)
+from tests.utils.text_parsing import CHARACTER_MAP_RESTRAINED
 
 
 @pytest.mark.parametrize(
@@ -34,3 +40,33 @@ def test_apu_reset(path_rom: str) -> None:
 def test_apu_test(path_rom: str) -> None:
     """Run the apu_test test suite."""
     run_test_rom_ram(path_rom=path_rom)
+
+@pytest.mark.parametrize(
+    "path_rom", [
+        "blargg_apu_2005.07.30/01.len_ctr.nes",
+        "blargg_apu_2005.07.30/02.len_table.nes",
+        "blargg_apu_2005.07.30/03.irq_flag.nes",
+        "blargg_apu_2005.07.30/04.clock_jitter.nes",
+        "blargg_apu_2005.07.30/05.len_timing_mode0.nes",
+        "blargg_apu_2005.07.30/06.len_timing_mode1.nes",
+        "blargg_apu_2005.07.30/07.irq_flag_timing.nes",
+        "blargg_apu_2005.07.30/08.irq_timing.nes",
+        "blargg_apu_2005.07.30/09.reset_timing.nes",
+        "blargg_apu_2005.07.30/10.len_halt_timing.nes",
+        "blargg_apu_2005.07.30/11.len_reload_timing.nes",
+    ]
+)
+def test_blargg_apu_2005_07_30(path_rom: str) -> None:
+    """Run the blargg_apu_2005.07.30 test suite."""
+    run_test_rom_ppu(
+        path_rom=path_rom,
+        success_matcher=Matcher(
+            string="$01",
+            condition=MatchCondition.LAST_LINE_STRICT
+        ),
+        failure_matcher=Matcher(
+            string=r"\$(?!01\b)\d{2}",
+            condition=MatchCondition.REGEX
+        ),
+        character_map=CHARACTER_MAP_RESTRAINED
+    )
